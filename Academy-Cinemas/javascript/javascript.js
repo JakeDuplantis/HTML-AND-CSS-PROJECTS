9a4d09e318af79e18f3775b2aaa0fa573d4bd82c
Jacob Duplantis <duplantisjacob54@gmail.com>
2026-09-08 16:01:51 -0500

    Updated Academy-Cinemas
    Updated Academy-Cinemas to include shrink and scroll effect. Mobile menu automatically closes when link is clicked.

--- Academy-Cinemas/javascript/javascript.js
+++ Academy-Cinemas/javascript/javascript.js
@@ -39,3 +39,37 @@ function displaySelectedMovieOptions() {
 function buyTickets() {
     displaySelectedMovieOptions();
 }
+
+//JQUERY
+//shrinks header size when the document is scrolled down by 80 pixels
+$(document).on("scroll", function () {
+    //When the webpage is scrolled down from the top by 50px this
+    //if statement will trigger
+    if ($(document).scroll() > 50) {
+        //Once the 50px requirement has been met add the 
+        //nav-shrink class selector to the same HTML element
+        //that had the nav class
+        $("nav").addClass("nav-shrink");
+        //Adjust the position of the mobile drop menu
+        //to accomodate the new hieght decrease
+        $("div.navbar-collapse").css("margin-top", "-6px");
+    } else {
+        //if the webpage has not been scrolled down or
+        //is back at the top the nav-shrink class selector
+        //is removed from the HTML element with the nav
+        //class selector
+        $("nav").removeClass("nav-shrink");
+        //The margin for the drop down menu is now returned to it's original amount
+        $("div.navbar-collapse").css("margin-top", "14px");
+    }
+});
+
+//close mobile menu when a navigation link is clicked
+$(document).ready(function () {
+    //on click when an element contains just the nav-link class and not the dropdown-toggle and then
+    //also close when an element with the class .dropdown-item (each movie link) has been clickeded
+    $(".navbar-nav").on('click', '.nav-link:not(".dropdown-toggle"), .dropdown-item', function() {
+        //collapse the navbar when a link or dropdown tiem is clicked
+        $(".navbar-collapse").collapse('hide');
+    });
+});
